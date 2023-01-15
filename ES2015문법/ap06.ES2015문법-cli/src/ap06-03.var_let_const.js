80cf756a42489d397945fa69a18f2cb1d443a79c
an9383 <an9383@gmail.com>
2023-01-15 12:12:26 +0900

    t

--- ES2015문법/ap06.ES2015문법-cli/src/ap06-03.var_let_const.js
+++ ES2015문법/ap06.ES2015문법-cli/src/ap06-03.var_let_const.js
@@ -25,3 +25,20 @@
     변수 중복 선언 불가
     변수 이름 충돌 불가
 */
+
+let msg = 'global scope';
+
+if (true) {
+  let msg = 'block scope';
+}
+
+function outer(params) {
+  let msg = 'function scope at outer ';
+
+  if (true) {
+    let msg = 'block scope at outer ';
+  }
+}
+
+const pizza = true;
+pizza = false; // 에러 발생.
