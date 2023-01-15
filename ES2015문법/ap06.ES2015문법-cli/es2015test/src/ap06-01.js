3477cb6dc01d269cef72433ea569fab04e0a821b
an9383 <an9383@gmail.com>
2023-01-15 11:36:26 +0900

    t

--- ES2015문법/ap06.ES2015문법-cli/es2015test/src/ap06-01.js
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/src/ap06-01.js
@@ -4,9 +4,9 @@ const name = 'world';
 console.log(`Hello ${name}`);
 
 const getMessage = (params) => {
-    console.log(`Hello ${params}`);
+  console.log(`Hello ${params}`);
 };
 getMessage('sejoong');
 
-const str = "aaaa";
+const str = 'aaaa';
 console.log(str);
