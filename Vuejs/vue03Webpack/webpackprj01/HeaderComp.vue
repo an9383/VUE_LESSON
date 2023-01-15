f1864c8fddbd7b535e573d92030034d60cf2aa75
choiyunmi-ui <chym8872@nate.com>
2023-01-15 10:26:41 +0900

    t

---
+++ Vuejs/vue03Webpack/webpackprj01/HeaderComp.vue
@@ -0,0 +1,17 @@
+<style scoped>
+header {
+  height: 60px;
+  background-color: #efb14e;
+  margin: 0 0 20px 0;
+}
+</style>
+
+<template>
+  <header data-role="header">
+    <h1>Header.vue</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
