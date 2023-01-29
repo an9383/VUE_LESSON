fe30540f04f6bb7f889e4746056fd50653ddc3b0
an9383 <an9383@gmail.com>
2023-01-29 10:24:57 +0900

    a

--- ES2015문법/ap06.ES2015문법-cli/es2015test/src/Module1.js
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/src/Module1.js
@@ -1,4 +1,12 @@
-// Module1.js
+/*
+
+  export 변수나 함수를 내보낼 때 사용한다.
+  export 파일에서 여러 번 사용 가능하다.
+
+  exprot default 변수나 함수를 내보낼 때 사용한다.
+  export default 는 파일에서 한 번만 사용 가능하다.
+*/
+
 const a = 1;
 const b = 2;
 
--- ES2015문법/ap06.ES2015문법-cli/es2015test/src/ap06-15.template_literal.js
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/src/ap06-15.template_literal.js
@@ -1,7 +1,9 @@
 /*
 
-    ES2015의 template literal( 백틱, ``) 에 대해서 알아본다.
-
+  ES2015의 template literal( 백틱, ` ) 에 대해서 알아본다.
+  template literal 의 용도
+  1. 다중 문자열 만들 때
+  2. 변수 치환
 */
 const string1 = '안녕하세요';
 const string2 = '반갑습니다';
