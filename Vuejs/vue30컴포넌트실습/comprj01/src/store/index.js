0ecfcd6589c6d690c0e320b9c453b3fcb0f2db0f
an9383 <an9383@gmail.com>
2023-02-05 16:55:16 +0900

    a

--- Vuejs/vue30컴포넌트실습/comprj01/src/store/index.js
+++ Vuejs/vue30컴포넌트실습/comprj01/src/store/index.js
@@ -1,9 +1,14 @@
 import { createStore } from 'vuex';
 
+import counterStore from './modules/counterStore';
+
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  // state: {},
+  // getters: {},
+  // mutations: {},
+  // actions: {},
+  modules: {
+    // "모듈명: store명", 형태로 저장됩니다.
+    counterStore: counterStore,
+  },
 });
