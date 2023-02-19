6e7cf5b1c994e6cdc0a6464216094878fc13717c
an9383 <an9383@gmail.com>
2023-02-19 17:14:34 +0900

    t

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,30 @@ module.exports = {
   /*
     https://feynubrick.github.io/2019/05/20/eslint-prettier.html
     ESLint 규칙:
-        https://eslint.org/docs/rules/
-        https://eslint.org/docs/user-guide/configuring/
+      https://eslint.org/docs/rules/
+      https://eslint.org/docs/user-guide/configuring/
     Prettier 옵션:
-        https://prettier.io/docs/en/options.html
-    */
+      https://prettier.io/docs/en/options.html
+  */
   root: true,
   env: {
-    browser: false,
     node: true,
+
+    browser: true,
+    commonjs: true,
+    es6: true,
+    es2021: true,
+    jest: true,
     mocha: true,
   },
-  plugins: ['prettier'],
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
   },
+  plugins: ['prettier'],
   ignorePatterns: ['node_modules/'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
@@ -29,4 +37,12 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
