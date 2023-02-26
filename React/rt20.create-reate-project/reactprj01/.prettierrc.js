bb08555cafe09256032a4931be027402957eac47
an9383 <an9383@gmail.com>
2023-02-26 10:22:08 +0900

    t

--- React/rt20.create-reate-project/reactprj01/.prettierrc.js
+++ React/rt20.create-reate-project/reactprj01/.prettierrc.js
@@ -2,7 +2,7 @@
 // .json 파일 주석이 안된다.
 module.exports = {
   /* https://prettier.io/docs/en/options.html */
-  printWidth: 130 /* 한 줄에 130컬럼까지 허용 */,
+  printWidth: 80 /* 한 줄에 80컬럼까지 허용 */,
   tabWidth: 2 /* tab을 누르면 2칸 들여쓰기 */,
   singleQuote: true /*문자열 리터럴로 " 대신 ' 을 사용 */,
   semi: true /* 문장 마지막에 ; 추가 */,
